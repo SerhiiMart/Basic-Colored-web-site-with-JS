a3a8ab31fa8de5fe09eae4058082732f2332c01a
Serhii <malgothsm@gmail.com>
2021-07-05 16:48:55 +0300

    For export

---
+++ DB-module.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+let dbName = "SPCDB"
+mongoose.connect(process.env.DB, {   
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  }
+);
+let db = mongoose.connection
+db.on('error', err => { console.error(err) });
+db.once('open', () => {
+  console.log(`Connected to Database ${dbName}`)
+});
+
+module.exports = db;
